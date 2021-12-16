c3edb0354659b8c20e8528853e01e9dec8635d83
huynth1801 <huuhuy1801@gmail.com>
2021-12-16 20:35:04 +0700

    add summary model

--- models/discriminator.py
+++ models/discriminator.py
@@ -13,25 +13,25 @@ class Discriminator(nn.Module):
         self.nc = nc
         self.ndf = ndf
         self.main = nn.Sequential(
-                    # (nc)x128x128
+                    # Block 1: (nc)x128x128
                     nn.Conv2d(nc, ndf, 4, 2, 1, bias=False),
                     nn.LeakyReLU(0.2, inplace=True),
-                    # state size (ndf)x64x64
+                    # Block 2: state size (ndf)x64x64
                     nn.Conv2d(ndf, ndf, 4, 2, 1, bias=False),
                     nn.LeakyReLU(0.2, inplace=True),
-                    # state size (ndf)x32x32
+                    # Block 3: state size (ndf)x32x32
                     nn.Conv2d(ndf, ndf*2 , 4, 2, 1, bias=False),
                     nn.BatchNorm2d(ndf*2),
                     nn.LeakyReLU(0.2, inplace=True),
-                    # state size (ndf*2)x16x16
+                    # Block 4: state size (ndf*2)x16x16
                     nn.Conv2d(ndf*2, ndf*4, 4, 2, 1, bias=False),
                     nn.BatchNorm2d(ndf*4),
                     nn.LeakyReLU(0.2, inplace=True),
-                    # state size (ndf*4)x8x8
+                    # Block 5: state size (ndf*4)x8x8
                     nn.Conv2d(ndf*4, ndf*8, 4, 2, 1, bias=False),
                     nn.BatchNorm2d(ndf*8),
                     nn.LeakyReLU(0.2, inplace=True),
-                    # state size. (ndf*8)x4x4
+                    # Block 6: state size. (ndf*8)x4x4
                     nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=False),
                     nn.Sigmoid()
         )
--- models/generator.py
+++ models/generator.py
@@ -16,26 +16,27 @@ class Generator(nn.Module):
         self.ngf = ngf
         self.nc = nc
         self.main = nn.Sequential(
+                    # Block 1
                     nn.ConvTranspose2d(nz, ngf*8, 4, 1, 0, bias=False),
                     nn.BatchNorm2d(ngf*8),
                     nn.ReLU(True),
-                    # state size (ngf*8)x4x4
+                    # Block 2: state size (ngf*8)x4x4
                     nn.ConvTranspose2d(ngf*8, ngf*4, 4, 2, 1, bias=False),
                     nn.BatchNorm2d(ngf*4),
                     nn.ReLU(True),
-                    # state size (ngf*4)x8x8
+                    # Block 3: state size (ngf*4)x8x8
                     nn.ConvTranspose2d(ngf*4, ngf*2, 4, 2, 1, bias=False),
                     nn.BatchNorm2d(ngf*2),
                     nn.ReLU(True),
-                    # state size (ngf*2)x16x16
+                    # Block 4: state size (ngf*2)x16x16
                     nn.ConvTranspose2d(ngf*2, ngf, 4, 2, 1, bias=False),
                     nn.BatchNorm2d(ngf),
                     nn.ReLU(True),
-                    # state size (ngf)x32x32
+                    # Block 5: state size (ngf)x32x32
                     nn.ConvTranspose2d(ngf, ngf//2, 4, 2, 1, bias=False),
                     nn.BatchNorm2d(ngf//2),
                     nn.ReLU(True),
-                    # state size (ngf//2)x64x64
+                    # Block 6: state size (ngf//2)x64x64
                     nn.ConvTranspose2d(ngf//2, nc, 4, 2, 1, bias=False),
                     nn.Tanh()
                     # state size. (nc) x 128 x 128
